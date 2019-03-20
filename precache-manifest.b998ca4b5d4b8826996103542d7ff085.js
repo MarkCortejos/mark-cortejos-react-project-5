4f955e0d3ab30e8ba61015ad0991b9f84f59eb57
MarkCortejos <markcortejos@gmail.com>
2019-03-20 01:46:12 -0400

    Updates

---
+++ precache-manifest.b998ca4b5d4b8826996103542d7ff085.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "b944a994d0378ae36d26",
+    "url": "/mark-cortejos-react-project-5/static/css/main.ef2f7643.chunk.css"
+  },
+  {
+    "revision": "b944a994d0378ae36d26",
+    "url": "/mark-cortejos-react-project-5/static/js/main.b944a994.chunk.js"
+  },
+  {
+    "revision": "9da31a7a43927ecb3b9c",
+    "url": "/mark-cortejos-react-project-5/static/js/runtime~main.9da31a7a.js"
+  },
+  {
+    "revision": "e9c4d65a00f657d3c279",
+    "url": "/mark-cortejos-react-project-5/static/js/2.e9c4d65a.chunk.js"
+  },
+  {
+    "revision": "4688d4108a3e8f0634b126730033d82d",
+    "url": "/mark-cortejos-react-project-5/static/media/DnD_Logo.4688d410.png"
+  },
+  {
+    "revision": "cc42099b19996d043583e8a4377e8aae",
+    "url": "/mark-cortejos-react-project-5/static/media/paperBody.cc42099b.png"
+  },
+  {
+    "revision": "57f6ea113ab8b27188c4f496543efb4f",
+    "url": "/mark-cortejos-react-project-5/static/media/paperBottom.57f6ea11.png"
+  },
+  {
+    "revision": "00176b2c5dad6e2e723e828b8f5895ed",
+    "url": "/mark-cortejos-react-project-5/static/media/dndbkg.00176b2c.jpg"
+  },
+  {
+    "revision": "e0139226fb6eb14f6009f4e87fa2f8c3",
+    "url": "/mark-cortejos-react-project-5/static/media/paperTop.e0139226.png"
+  },
+  {
+    "revision": "7d23e77b1a1116dca6673564948dea41",
+    "url": "/mark-cortejos-react-project-5/index.html"
+  }
+];
